898c51d31e485e8d7ce2111f1bc0761179b712af
André <andrefm06@gmail.com>
2026-06-19 07:14:41 -0300

    Initial commit.

---
+++ Paradigmas/Aluno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paradigmas
+{
+    internal class Aluno : IPessoa
+    {
+        public string Nome {  get; set; }
+        public int Idade { get; set; }
+
+
+        public Aluno(string Nome, int Idade)
+        {
+            this.Nome = Nome;
+            this.Idade = Idade;
+        }
+        public void preencher()
+        {
+            Console.WriteLine($"Teste");
+        }
+    }
+}
---
+++ Paradigmas/IPessoa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paradigmas
+{
+    public interface IPessoa
+    {
+        string Nome { get; set; }
+        int Idade {  get; set; }
+        void preencher();
+    }
+}
